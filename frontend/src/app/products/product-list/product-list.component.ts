aeec821e6a7ef7a5ab2d4db08fa198d26740153d
atharvguptaaa <workinatharv@gmail.com>
2025-03-08 16:10:00 +0530

    frontend progress

--- frontend/src/app/products/product-list/product-list.component.ts
+++ frontend/src/app/products/product-list/product-list.component.ts
@@ -1,11 +1,13 @@
+// product-list.component.ts
 import { Component, OnInit } from '@angular/core';
 import { Product } from '../../models/product-model';
 import { ProductService } from '../../services/product.service';
 import { NgFor } from '@angular/common';
+import { ProductCardComponent } from "../product-card/product-card.component";
 
 @Component({
   selector: 'app-product-list',
-  imports: [NgFor],
+  imports: [NgFor, ProductCardComponent],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.scss'
 })
@@ -16,12 +18,11 @@ export class ProductListComponent implements OnInit {
 
   ngOnInit(): void {
       this.productService.getProducts().subscribe((data)=>{
-        this.products=data;
+          this.products=data;
       })
   }
 
   addToCart(product:Product){
-    
+    console.log("Product Added to cart : " , product);
   }
-
 }
--- frontend/src/app/services/product.service.ts
+++ frontend/src/app/services/product.service.ts
@@ -7,7 +7,7 @@ import { Product } from '../models/product-model';
   providedIn: 'root'
 })
 export class ProductService {
-  private apiUrl='http://localhost:3000/products';
+  private apiUrl='http://localhost:3000/product';
   
   constructor(private http:HttpClient) { }
 
